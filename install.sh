1ad35d6db25f59cace04cf6cc15d0d39242d64cb
johabu <johabu96@yahoo.de>
2012-02-05 19:22:44 +0100

    Added Readme file, license text, install script and modified texinit.c

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo
+echo "gcc -O2 -o texinit -W -Wall texinit.c"
+echo "Start compiling ..."
+gcc -O2 -o texinit -W -Wall texinit.c
+echo
+echo "compiled."
+echo
--- texinit.c
+++ texinit.c
@@ -1,9 +1,24 @@
+/*
+*	texinit.c
+*	Developed by Johabu <http://johabu.spheniscida.de>; <http://github.com/johabu>
+*	
+	This file is part of texinit.
+*	texinit is free software; you can modify it or redistribute it
+*	under the terms of the GNU General Public License as published by
+*	the Free Software Foundation <http://www.fsf.org>, either version 3,
+*	or (at your option) any later version.
+*	See <http://www.gnu.org/licenses> for the license, if you
+*	haven't received a copy of it (GNU_GPL.txt).
+*	
+*	texinit is distributed without any warranty!
+*/
+
 #include <stdlib.h>
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
 	FILE *datei;
-	printf("Open file %s ...",argv[1]);
+	printf("Open file: \"%s\" ...",argv[1]);
 	datei = fopen(argv[1], "w+");
 	if (datei == NULL) {
 		printf("\nError in opening\n");
